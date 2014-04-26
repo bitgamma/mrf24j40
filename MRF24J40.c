1f5d31ef75a015219376643d3fcef1cb09bd5c28
Michele Balistreri <michele@briksoftware.com>
2014-04-26 10:52:24 +0200

    First import

---
+++ MRF24J40.c
@@ -0,0 +1,428 @@
+/*
+ * Copyright (C) 2014, Michele Balistreri
+ *
+ * Derived from code originally Copyright (C) 2011, Alex Hornung
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a 
+ * copy of this software and associated documentation files (the "Software"), 
+ * to deal in the Software without restriction, including without limitation 
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the 
+ * Software is furnished to do so, subject to the following conditions: 
+ *
+ * The above copyright notice and this permission notice shall be included in 
+ * all copies or substantial portions of the Software. 
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL 
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+#include "MRF24J40.h"
+
+#include <xc.h>
+#include <stdlib.h>
+#include <plib/spi.h>
+
+static int internal_state = 0;
+
+#define MRF24J40_SPI_PREAMBLE() volatile unsigned char tmpIE = MRF24J40_IE; MRF24J40_IE = 0; MRF24J40_CS = 0;
+#define MRF24J40_SPI_POSTAMBLE() MRF24J40_CS = 1; MRF24J40_IE = tmpIE;
+
+void _mrf24j40_write_long_addr(unsigned short addr, unsigned char write) {
+  MRF24J40_SPI_WRITE(((addr >> 3) & 0x7F) | 0x80);
+  MRF24J40_SPI_WRITE(((addr << 5) & 0xE0) | (write << 4));
+}
+
+void _mrf24j40_write_short_addr(unsigned char addr, unsigned char write) {
+  MRF24J40_SPI_WRITE(((addr << 1) & 0x7E) | write);
+}
+
+unsigned char mrf24j40_read_long_ctrl_reg(unsigned short addr) {
+  MRF24J40_SPI_PREAMBLE();
+  _mrf24j40_write_long_addr(addr, 0);
+  unsigned char value = MRF24J40_SPI_READ();
+  MRF24J40_SPI_POSTAMBLE();
+
+  return value;
+}
+
+unsigned char mrf24j40_read_short_ctrl_reg(unsigned char addr) {
+  MRF24J40_SPI_PREAMBLE();
+  _mrf24j40_write_short_addr(addr, 0);
+  unsigned char value = MRF24J40_SPI_READ();
+  MRF24J40_SPI_POSTAMBLE();
+  return value;
+}
+
+void mrf24j40_write_long_ctrl_reg(unsigned short addr, unsigned char value) {
+  MRF24J40_SPI_PREAMBLE();
+  _mrf24j40_write_long_addr(addr, 1);
+  MRF24J40_SPI_WRITE(value);
+  MRF24J40_SPI_POSTAMBLE();
+}
+
+void mrf24j40_write_short_ctrl_reg(unsigned char addr, unsigned char value) {
+  MRF24J40_SPI_PREAMBLE();
+  _mrf24j40_write_short_addr(addr, 1);
+  MRF24J40_SPI_WRITE(value);
+  MRF24J40_SPI_POSTAMBLE();
+}
+
+void mrf24j40_ie(void) {
+  mrf24j40_write_short_ctrl_reg(MRF24J40_INTCON, ~(TXNIE | RXIE | SECIE));
+}
+
+void mrf24j40_pwr_reset(void) {
+  mrf24j40_write_short_ctrl_reg(SOFTRST, RSTPWR);
+}
+
+void mrf24j40_bb_reset(void) {
+  mrf24j40_write_short_ctrl_reg(SOFTRST, RSTBB);
+}
+
+void mrf24j40_mac_reset(void) {
+  internal_state = 0;
+  mrf24j40_write_short_ctrl_reg(SOFTRST, RSTMAC);
+}
+
+void mrf24j40_rf_reset(void) {
+  unsigned char old = mrf24j40_read_short_ctrl_reg(RFCTL);
+
+  mrf24j40_write_short_ctrl_reg(RFCTL, old | RFRST);
+  mrf24j40_write_short_ctrl_reg(RFCTL, old & ~RFRST);
+  __delay_us(192);
+}
+
+void mrf24j40_rxfifo_flush(void) {
+  mrf24j40_write_short_ctrl_reg(RXFLUSH, (mrf24j40_read_short_ctrl_reg(RXFLUSH) | _RXFLUSH));
+}
+
+void mrf24j40_set_channel(int ch) {
+  mrf24j40_write_long_ctrl_reg(RFCON0, CHANNEL(ch) | RFOPT(0x03));
+  mrf24j40_rf_reset();
+}
+
+unsigned char mrf24j40_get_channel(void) {
+  return (11 + (mrf24j40_read_long_ctrl_reg(RFCON0) >> 4));
+}
+
+void mrf24j40_set_promiscuous(int crc_check) {
+  unsigned char w = NOACKRSP;
+  if (!crc_check) {
+    w |= ERRPKT;
+  } else {
+    w |= PROMI;
+  }
+
+  mrf24j40_write_short_ctrl_reg(RXMCR, w);
+}
+
+void mrf24j40_set_coordinator(void) {
+  mrf24j40_write_short_ctrl_reg(RXMCR, mrf24j40_read_short_ctrl_reg(RXMCR) | PANCOORD);
+}
+
+void mrf24j40_clear_coordinator(void) {
+  mrf24j40_write_short_ctrl_reg(RXMCR, mrf24j40_read_short_ctrl_reg(RXMCR) & ~PANCOORD);
+}
+
+void mrf24j40_set_pan(unsigned char *pan) {
+  mrf24j40_write_short_ctrl_reg(PANIDL, pan[0]);
+  mrf24j40_write_short_ctrl_reg(PANIDH, pan[1]);
+}
+
+void mrf24j40_set_short_addr(unsigned char *addr) {
+  mrf24j40_write_short_ctrl_reg(SADRL, addr[0]);
+  mrf24j40_write_short_ctrl_reg(SADRH, addr[1]);
+}
+
+void mrf24j40_set_eui(unsigned char *eui) {
+  mrf24j40_write_short_ctrl_reg(EADR0, eui[0]);
+  mrf24j40_write_short_ctrl_reg(EADR1, eui[1]);
+  mrf24j40_write_short_ctrl_reg(EADR2, eui[2]);
+  mrf24j40_write_short_ctrl_reg(EADR3, eui[3]);
+  mrf24j40_write_short_ctrl_reg(EADR4, eui[4]);
+  mrf24j40_write_short_ctrl_reg(EADR5, eui[5]);
+  mrf24j40_write_short_ctrl_reg(EADR6, eui[6]);
+  mrf24j40_write_short_ctrl_reg(EADR7, eui[7]);
+}
+
+void mrf24j40_hard_reset() {
+  MRF24J40_RESET = 0;
+
+  internal_state = 0;
+  __delay_us(192);
+
+  MRF24J40_RESET = 1;
+
+  __delay_us(192);
+}
+
+void mrf24j40_init(int ch) {
+  MRF24J40_OPEN_SPI();
+  MRF24J40_WAKE = 0;
+  
+  mrf24j40_hard_reset();
+  
+  mrf24j40_write_short_ctrl_reg(SOFTRST, (RSTPWR | RSTBB | RSTMAC));
+
+  __delay_us(192);
+
+  mrf24j40_write_short_ctrl_reg(PACON2, FIFOEN | TXONTS(0x18));
+  mrf24j40_write_short_ctrl_reg(TXSTBL, RFSTBL(9) | MSIFS(5));
+  mrf24j40_write_long_ctrl_reg(RFCON0, CHANNEL(ch) | RFOPT(0x03));
+  mrf24j40_write_long_ctrl_reg(RFCON1, VCOOPT(0x01));
+  mrf24j40_write_long_ctrl_reg(RFCON2, PLLEN);
+  mrf24j40_write_long_ctrl_reg(RFCON6, _20MRECVR);
+  mrf24j40_write_long_ctrl_reg(RFCON7, SLPCLKSEL(0x02));
+  mrf24j40_write_long_ctrl_reg(RFCON8, RFVCO);
+  mrf24j40_write_long_ctrl_reg(SLPCON1, SLPCLKDIV(1) | CLKOUTDIS);
+
+  mrf24j40_write_short_ctrl_reg(BBREG2, CCAMODE(0x02) | CCASTH(0x00));
+  mrf24j40_write_short_ctrl_reg(CCAEDTH, 0x60);
+
+  mrf24j40_write_short_ctrl_reg(BBREG6, RSSIMODE2);
+
+  mrf24j40_rxfifo_flush();
+  
+  mrf24j40_ie();
+
+  mrf24j40_rf_reset();
+}
+
+void mrf24j40_sleep(int spi_wake) {
+  mrf24j40_write_short_ctrl_reg(WAKECON, IMMWAKE);
+
+  unsigned char r = mrf24j40_read_short_ctrl_reg(SLPACK);
+
+  if (!spi_wake) {
+    MRF24J40_WAKE = 0;
+    mrf24j40_write_short_ctrl_reg(RXFLUSH, mrf24j40_read_short_ctrl_reg(RXFLUSH) | WAKEPAD | WAKEPOL);
+  }
+
+  mrf24j40_pwr_reset();
+  mrf24j40_write_short_ctrl_reg(SLPACK, r | _SLPACK);
+}
+
+void mrf24j40_wakeup(int spi_wake) {
+  if (spi_wake) {
+    mrf24j40_write_short_ctrl_reg(WAKECON, REGWAKE);
+    mrf24j40_write_short_ctrl_reg(WAKECON, 0);
+  } else {
+    MRF24J40_WAKE = 1;
+  }
+
+  mrf24j40_rf_reset();
+}
+
+void mrf24j40_set_encdec(int types, int mode, unsigned char *key, int klen) {
+  unsigned char *keyp;
+
+  unsigned char w = mrf24j40_read_short_ctrl_reg(SECCON0);
+
+  if (types & MRF24J40_TX_KEY) {
+    MRF24J40_SPI_PREAMBLE();
+    _mrf24j40_write_long_addr(SECKTXNFIFO, 1);
+
+    keyp = key;
+
+    int len = klen;
+
+    while (len-- > 0) {
+      MRF24J40_SPI_WRITE(*keyp++);
+    }
+
+    MRF24J40_SPI_POSTAMBLE();
+
+
+    w |= TXNCIPHER(mode);
+    mrf24j40_write_short_ctrl_reg(SECCON0, w);
+  }
+
+  if (types & MRF24J40_RX_KEY) {
+    MRF24J40_SPI_PREAMBLE();
+    _mrf24j40_write_long_addr(SECKRXFIFO, 1);
+
+    keyp = key;
+    int len = klen;
+
+    while (len-- > 0) {
+      MRF24J40_SPI_WRITE(*keyp++);
+    }
+
+    MRF24J40_SPI_POSTAMBLE();
+
+    w |= RXCIPHER(mode);
+    mrf24j40_write_short_ctrl_reg(SECCON0, w);
+  }
+}
+
+void mrf24j40_txpkt(unsigned char *frame, int hdr_len, int payload_len) {
+  internal_state = 0;
+  int frame_len = hdr_len+payload_len;
+
+  MRF24J40_SPI_PREAMBLE();
+  _mrf24j40_write_long_addr(TXNFIFO, 1);
+  MRF24J40_SPI_WRITE(hdr_len);
+  MRF24J40_SPI_WRITE(frame_len);
+
+  while (frame_len-- > 0) {
+    MRF24J40_SPI_WRITE(*frame++);
+  }
+  
+  MRF24J40_SPI_POSTAMBLE();
+
+  unsigned char w = mrf24j40_read_short_ctrl_reg(TXNCON);
+  w &= ~(TXNSECEN);
+
+  if (IEEE_802_15_4_HAS_SEC(frame[0])) {
+    w |= TXNSECEN;
+  }
+
+  if (IEEE_802_15_4_WANTS_ACK(frame[0])) {
+    w |= TXNACKREQ;
+  }
+
+  mrf24j40_write_short_ctrl_reg(TXNCON, w | TXNTRIG);
+}
+
+int mrf24j40_txpkt_intcb(void) {
+  unsigned char stat = mrf24j40_read_short_ctrl_reg(TXSTAT);
+  if (stat & TXNSTAT) {
+    if (stat & CCAFAIL) {
+      return EBUSY;
+    } else {
+      return EIO;
+    }
+  } else {
+    return 0;
+  }
+}
+
+int mrf24j40_sec_intcb(int accept) {
+  unsigned char w = mrf24j40_read_short_ctrl_reg(SECCON0);
+  
+  w &= ~(SECSTART | SECIGNORE);
+
+  if (accept) {
+    mrf24j40_write_short_ctrl_reg(SECCON0, w | SECSTART);
+  } else {
+    mrf24j40_write_short_ctrl_reg(SECCON0, w | SECIGNORE);
+    mrf24j40_write_short_ctrl_reg(RXFLUSH, _RXFLUSH);
+  }
+}
+
+int mrf24j40_check_rx_dec(int no_err_flush) {
+  int err = (mrf24j40_read_short_ctrl_reg(RXSR) & SECDECERR) ? EIO : 0;
+
+  if (err && !no_err_flush) {
+    mrf24j40_write_short_ctrl_reg(RXFLUSH, _RXFLUSH);
+  }
+
+  return err;
+}
+
+int mrf24j40_rxpkt_intcb(unsigned char *buf, unsigned char *plqi, unsigned char *prssi) {
+  mrf24j40_write_short_ctrl_reg(BBREG1, mrf24j40_read_short_ctrl_reg(BBREG1) | RXDECINV);
+
+  MRF24J40_SPI_PREAMBLE();
+  _mrf24j40_write_long_addr(RXFIFO, 0);
+
+  unsigned int flen = MRF24J40_SPI_READ();
+
+  for (unsigned int i = 0; i < flen; i++) {
+    *buf++ = MRF24J40_SPI_READ();
+  }
+
+  unsigned char lqi = MRF24J40_SPI_READ();
+  unsigned char rssi = MRF24J40_SPI_READ();
+
+  if (plqi != NULL) {
+    *plqi = lqi;
+  }
+
+  if (prssi != NULL) {
+    *prssi = rssi;
+  }
+
+  MRF24J40_SPI_POSTAMBLE();
+
+  mrf24j40_rxfifo_flush();
+  mrf24j40_write_short_ctrl_reg(BBREG1, mrf24j40_read_short_ctrl_reg(BBREG1) & ~RXDECINV);
+  
+  return flen;
+}
+
+int mrf24j40_check_enc(void) {
+  return mrf24j40_txpkt_intcb();
+}
+
+int mrf24j40_check_dec(void) {
+  int error;
+
+  if ((error = mrf24j40_txpkt_intcb()) != 0) {
+    return error;
+  }
+
+  unsigned char w = mrf24j40_read_short_ctrl_reg(RXSR);
+  return (w & UPSECERR) ? EIO : 0;
+}
+
+int mrf24j40_int_tasks(void) {
+  int ret = 0;
+
+  unsigned char stat = mrf24j40_read_short_ctrl_reg(INTSTAT);
+
+  if (stat & RXIF) {
+    ret |= MRF24J40_INT_RX;
+  }
+
+  if (stat & TXNIF) {
+    switch (internal_state) {
+      case MRF24J40_STATE_UPENC:
+        ret |= MRF24J40_INT_ENC;
+        internal_state = 0;
+        break;
+
+      case MRF24J40_STATE_UPDEC:
+        ret |= MRF24J40_INT_DEC;
+        internal_state = 0;
+        break;
+
+      default:
+        ret |= MRF24J40_INT_TX;
+    }
+  }
+
+  if (stat & SECIF) {
+    ret |= MRF24J40_INT_SEC;
+  }
+
+  return ret;
+}
+
+void mrf24j40_encdec(unsigned char *nonce, int nonce_len, unsigned char *frame, int hdr_len, int payload_len, int enc) {
+  if (enc) {
+    internal_state = MRF24J40_STATE_UPENC;
+  } else {
+    internal_state = MRF24J40_STATE_UPDEC;
+  }
+
+  int addr = UPNONCE0;
+  while (nonce_len-- > 0) {
+    mrf24j40_write_long_ctrl_reg(addr++, *nonce++);
+  }
+
+  if (enc) {
+    mrf24j40_write_short_ctrl_reg(SECCR2, UPENC);
+  } else {
+    mrf24j40_write_short_ctrl_reg(SECCR2, UPDEC);
+  }
+
+  mrf24j40_txpkt(frame, hdr_len, payload_len);
+}
+
---
+++ MRF24J40.h
@@ -0,0 +1,363 @@
+/* 
+ * Copyright (C) 2014, Michele Balistreri
+ *
+ * Derived from code originally Copyright (C) 2011, Alex Hornung
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a 
+ * copy of this software and associated documentation files (the "Software"), 
+ * to deal in the Software without restriction, including without limitation 
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the 
+ * Software is furnished to do so, subject to the following conditions: 
+ *
+ * The above copyright notice and this permission notice shall be included in 
+ * all copies or substantial portions of the Software. 
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL 
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+#ifndef MRF24J40_H
+#define	MRF24J40_H
+
+#include "config.h"
+
+/* Return values */
+#define MRF24J40_INT_RX		0x01
+#define MRF24J40_INT_TX		0x02
+#define MRF24J40_INT_SEC	0x04
+#define MRF24J40_INT_SLP	0x08
+#define MRF24J40_INT_ENC	0x10
+#define MRF24J40_INT_DEC	0x20
+
+#define EIO			5
+#define EBUSY			16
+
+/* IEEE 802.15.4 constants needed for some flags */
+#define IEEE_802_15_4_HAS_SEC(x)      ((x >> 3) & 0x01)
+#define IEEE_802_15_4_WANTS_ACK(x)     ((x >> 5) & 0x01)
+
+/* Internal state */
+#define MRF24J40_STATE_UPENC	0x01
+#define MRF24J40_STATE_UPDEC	0x02
+
+/* enc dec parameters */
+#define MRF24J40_TX_KEY		0x01
+#define MRF24J40_RX_KEY		0x02
+#define MRF24J40_UP_KEY		MRF24J40_TX_KEY
+
+#define MRF24J40_AES_CBC_MAC32	0x07
+#define MRF24J40_AES_CBC_MAC64	0x06
+#define MRF24J40_AES_CBC_MAC128	0x05
+#define MRF24J40_AES_CCM32	0x04
+#define MRF24J40_AES_CCM64	0x03
+#define MRF24J40_AES_CCM128	0x02
+#define MRF24J40_AES_CTR	0x01
+#define MRF24J40_ALGO_NONE	0x00
+
+/* Short Address Control Register Map */
+#define RXMCR		0x00
+#define PANIDL		0x01
+#define PANIDH		0x02
+#define	SADRL		0x03
+#define SADRH		0x04
+#define EADR0		0x05
+#define EADR1		0x06
+#define EADR2		0x07
+#define EADR3		0x08
+#define EADR4		0x09
+#define EADR5		0x0A
+#define EADR6		0x0B
+#define EADR7		0x0C
+#define RXFLUSH		0x0D
+
+#define ORDER		0x10
+#define TXMCR		0x11
+#define ACKTMOUT	0x12
+#define ESLOTG1		0x13
+#define SYMTICKL	0x14
+#define SYMTICKH	0x15
+#define PACON0		0x16
+#define PACON1		0x17
+#define PACON2		0x18
+#define TXBCON0		0x1A
+#define TXNCON		0x1B
+#define TXG1CON		0x1C
+#define TXG2CON		0x1D
+#define ESLOTG23	0x1E
+#define ESLOTG45	0x1F
+
+#define ESLOTG67	0x20
+#define TXPEND		0x21
+#define WAKECON		0x22
+#define FRMOFFSET	0x23
+#define TXSTAT		0x24
+#define TXBCON1		0x25
+#define GATECLK		0x26
+#define TXTIME		0x27
+#define HSYMTMRL	0x28
+#define HSYMTMRH	0x29
+#define SOFTRST		0x2A
+#define SECCON0		0x2c
+#define SECCON1		0x2d
+#define TXSTBL		0x2e
+
+#define RXSR		0x30
+#define INTSTAT		0x31
+#define MRF24J40_INTCON 0x32
+#define GPIO		0x33
+#define TRISGPIO	0x34
+#define SLPACK		0x35
+#define RFCTL		0x36
+#define SECCR2		0x37
+#define BBREG0		0x38
+#define BBREG1		0x39
+#define BBREG2		0x3A
+#define BBREG3		0x3B
+#define BBREG4		0x3C
+#define BBREG6		0x3E
+#define CCAEDTH		0x3F
+
+/* Long Address Control Register Map */
+#define RFCON0		0x200
+#define RFCON1		0x201
+#define RFCON2		0x202
+#define RFCON3		0x203
+#define RFCON5		0x205
+#define RFCON6		0x206
+#define RFCON7		0x207
+#define RFCON8		0x208
+#define RFSTATE		0x20F
+
+#define RSSI		0x210
+#define SLPCON0		0x211
+#define SLPCON1		0x220
+
+#define WAKETIMEL	0x222
+#define WAKETIMEH	0x223
+
+#define MAINCNT0	0x226
+#define MAINCNT1	0x227
+#define MAINCNT2	0x228
+#define MAINCNT3	0x229
+
+#define UPNONCE0	0x240
+#define UPNONCE1	0x241
+#define UPNONCE2	0x242
+#define UPNONCE3	0x243
+#define UPNONCE4	0x244
+#define UPNONCE5	0x245
+#define UPNONCE6	0x246
+#define UPNONCE7	0x247
+#define UPNONCE8	0x248
+#define UPNONCE9	0x249
+#define UPNONCE10	0x24A
+#define UPNONCE11	0x24B
+#define UPNONCE12	0x24C
+
+/* Long Address Memory Map */
+#define TXNFIFO		0x000 /* - 0x07F, 128 bytes */
+#define TXBFIFO		0x080 /* - 0x0FF, 128 bytes */
+#define TXG1FIFO	0x100 /* - 0x17F, 128 bytes */
+#define TXG2FIFO	0x180 /* - 0x1FF, 128 bytes */
+#define SECKFIFO	0x280 /* - 0x2BF, 64 bytes */
+#define SECKTXNFIFO	0x280 /* - 0x28F, 16 bytes */
+#define SECKRXFIFO	0x2B0 /* - 0x2BF, 16 bytes */
+#define RXFIFO		0x300 /* - 0x38F, 128 bytes */
+
+
+/* RXMCR */
+#define NOACKRSP	(1<<5)
+#define PANCOORD	(1<<3)
+#define COORD		(1<<2)
+#define ERRPKT		(1<<1)
+#define PROMI		(1)
+
+/* RXFLUSH */
+#define WAKEPOL		(1<<6)
+#define WAKEPAD		(1<<5)
+#define CMDONLY		(1<<3)
+#define DATAONLY	(1<<2)
+#define BCNONLY		(1<<1)
+#define _RXFLUSH	(1)
+
+/* TXMCR */
+#define NOCSMA		(1<<7)
+#define BATLIFEXT	(1<<6)
+#define SLOTTED		(1<<5)
+#define MACMINBE(x)	((x & 0x03)<<3)
+#define CSMABF(x)	(x & 0x07)
+
+/* ACKTMOUT */
+#define DRPACK		(1<<7)
+
+/* PACON2 */
+#define FIFOEN		(1<<7)
+#define TXONTS(x)       (x & 0x3F)
+
+/* TXNCON */
+#define FPSTAT		(1<<4)
+#define INDIRECT	(1<<3)
+#define TXNACKREQ	(1<<2)
+#define TXNSECEN	(1<<1)
+#define TXNTRIG		(1)
+
+/* TXPEND */
+#define FPACK		(1)
+
+/* WAKECON */
+#define IMMWAKE		(1<<7)
+#define REGWAKE		(1<<6)
+
+/* TXSTAT */
+#define CCAFAIL		(1<<5)
+#define TXNSTAT		(1)
+
+/* SOFTRST */
+#define RSTPWR		(1<<2)
+#define RSTBB		(1<<1)
+#define RSTMAC		(1)
+
+/* SECCON0 */
+#define SECIGNORE	(1<<7)
+#define SECSTART	(1<<6)
+#define RXCIPHER(x)	((x & 0x07) << 3)
+#define TXNCIPHER(x)	((x & 0x07))
+
+/* SECCON1 */
+#define DISDEC		(1<<1)
+#define DISENC		(1)
+
+/* TXSTBL */
+#define RFSTBL(x)	((x & 0x0f) << 4)
+#define MSIFS(x)	((x & 0x0f))
+
+/* RXSR */
+#define UPSECERR	(1<<6)
+#define SECDECERR	(1<<2)
+
+
+/* INTSTAT */
+#define SLPIF		(1<<7)
+#define WAKEIF		(1<<6)
+#define HSYMTMRIF	(1<<5)
+#define SECIF		(1<<4)
+#define RXIF		(1<<3)
+#define TXG2IF		(1<<2)
+#define TXG1IF		(1<<1)
+#define TXNIF		(1)
+
+/* INTCON */
+#define SLPIE		(1<<7)
+#define WAKEIE		(1<<6)
+#define HSYMTMRIE	(1<<5)
+#define SECIE		(1<<4)
+#define RXIE		(1<<3)
+#define TXG2IE		(1<<2)
+#define TXG1IE		(1<<1)
+#define TXNIE		(1)
+
+/* SLPACK */
+#define _SLPACK		(1<<7)
+#define WAKECNT_L(x)	(x & 0x03F)
+
+/* RFCTL */
+#define WAKECNT_H(x)	((x & 0x03) << 3)
+#define RFRST		(1<<2)
+#define RFTXMODE	(1<<1)
+#define RFRXMODE	(1)
+
+/* SECCR2 */
+#define UPDEC		(1<<7)
+#define UPENC		(1<<6)
+
+/* BBREG0 */
+#define TURBO		(1)
+
+/* BBREG1 */
+#define RXDECINV	(1<<2)
+
+/* BBREG2 */
+#define CCAMODE(x)	((x & 0x03) <<6)
+#define CCASTH(x)	((x & 0x0F) <<2)
+
+/* BBREG3 */
+#define PREVALIDTH(x)	((x & 0x0F) <<4)
+
+/* BBREG4 */
+#define CSTH(x)		((x & 0x07) << 5)
+
+/* BBREG6 */
+#define RSSIMODE1	(1 << 7)
+#define RSSIMODE2	(1<<6)
+#define RSSIRDY		(1)
+
+/* RFCON0 */
+#define CHANNEL(x)	(((x - 11) & 0x0F) << 4)
+#define RFOPT(x)	((x & 0x0F))
+
+/* RFCON1 */
+#define VCOOPT(x)	((x & 0xFF))
+
+/* RFCON2 */
+#define PLLEN		(1<<7)
+
+/* RFCON3 */
+#define TXPWRL(x)	((x & 0x03) << 6)
+#define TXPWRS(x)	((x & 0x07) << 3)
+
+/* RFCON6 */
+#define TXFIL		(1 << 7)
+#define _20MRECVR       (1 << 4)
+#define BATEN           (1 << 3)
+
+/* RFCON 7 */
+#define SLPCLKSEL(x)	((x & 0x03) << 6)
+#define SLPCLKSEL_100k	(SLPCLKSEL(0x02))
+#define SLPCLKSEL_32k	(SLPCLKSEL(0x01))
+
+/* RFCON8 */
+#define RFVCO		(1 << 4)
+
+/* SLPCON0 */
+#define SLPCLKEN	(1)
+
+/* SLPCON1 */
+#define CLKOUTDIS	(1 << 5)	/* CLKOUTEN' */
+#define SLPCLKDIV(x)	((x & 0x1F))	/* division ratio: 2^(SLPCLKDIV) */
+
+
+unsigned char mrf24j40_read_long_ctrl_reg(unsigned short addr);
+unsigned char mrf24j40_read_short_ctrl_reg(unsigned char addr);
+void mrf24j40_write_long_ctrl_reg(unsigned short addr, unsigned char value);
+void mrf24j40_write_short_ctrl_reg(unsigned char addr, unsigned char value);
+void mrf24j40_rxfifo_flush(void);
+void mrf24j40_hard_reset(void);
+void mrf24j40_init(int ch);
+void mrf24j40_sleep(int spi_wake);
+void mrf24j40_wakeup(int spi_wake);
+void mrf24j40_set_short_addr(unsigned char *addr);
+void mrf24j40_set_eui(unsigned char *eui);
+void mrf24j40_set_pan(unsigned char *pan);
+void mrf24j40_set_channel(int ch);
+void mrf24j40_set_promiscuous(int crc_check);
+void mrf24j40_set_coordinator(void);
+void mrf24j40_clear_coordinator(void);
+void mrf24j40_txpkt(unsigned char *frame, int hdr_len, int payload_len);
+unsigned char mrf24j40_get_channel(void);
+int mrf24j40_int_tasks(void);
+int mrf24j40_rxpkt_intcb(unsigned char *buf, unsigned char *plqi, unsigned char *prssi);
+int mrf24j40_txpkt_intcb(void);
+int mrf24j40_sec_intcb(int accept);
+int mrf24j40_check_rx_dec(int no_err_flush);
+int mrf24j40_check_enc(void);
+int mrf24j40_check_dec(void);
+void mrf24j40_set_encdec(int types, int mode, unsigned char *key, int klen);
+void mrf24j40_encdec(unsigned char *nonce, int nonce_len, unsigned char *frame, int hdr_len, int payload_len, int enc);
+
+#endif /* MRF24J40_H */
